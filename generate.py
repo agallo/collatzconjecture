677b9e3389ed4f333a46d20fb237c2d8da08c405
Andrew Gallo <agallo@gwu.edu>
2015-09-14 11:16:11 -0400

    initial

--- generate.py
+++ generate.py
@@ -20,11 +20,11 @@ def sequence(start):
         while temp > 1:
             if temp % 2 == 0:
                 Ulist.append(processEven(temp))
-               # print Ulist[0],
+                print Ulist[0],
                 temp = Ulist[0]
             else:
                 Ulist.append(processOdd(temp))
-               # print Ulist[0],
+                print Ulist[0],
                 temp = Ulist[0]
         print Ulist
 
