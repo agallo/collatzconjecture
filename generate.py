04659edff888bf232c47a024d37643a9c3d4e271
Andrew Gallo <agallo@gwu.edu>
2015-09-14 10:22:02 -0400

    initial

---
+++ generate.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+
+__author__ = 'Andrew Gallo'
+
+
+def processEven(value):
+    return value / 2
+
+
+def processOdd(value):
+    return (3 * value) * 1
+
+
+def sequence(start):
+    for n in range (0, start):
+        temp = n
+        print temp,
+        while temp > 1:
+            Ulist = []
+            if temp % 2 == 0:
+                Ulist.append(processEven(temp))
+                print Ulist[0],
+                temp = Ulist[0]
+            else:
+                Ulist.append(processOdd(temp))
+                print Ulist[0],
+                temp = Ulist[0]
+        print
