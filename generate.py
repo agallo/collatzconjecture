0ad339b7fbec788e9693e77623d0066aaf9c6dbc
agallo <agallo@gwu.edu>
2016-08-20 19:34:35 -0400

    added option for different layout type

--- generate.py
+++ generate.py
@@ -3,43 +3,59 @@
 
 __author__ = 'Andrew Gallo'
 
+# FTO stands for four-two-one (always the end of the collatz conjecture)
+
 import networkx as nx
 import pygraphviz as pgv
+from argparse import ArgumentParser
 
+
+parser = ArgumentParser(description="calculate and graph numbers in the Collatz Conjecture")
+
+parser.add_argument('start', help="starting number", type=int)
+
+parser.add_argument('-g', '--graph', dest='gtype', type=str,
+                    help='graph type neato, dot[DEFAULT], twopi, circo, fdp, nop', default='dot')
+
+args = parser.parse_args()
+
+startnum = args.start
+gtype = args.gtype
+
+# list of all layout (progs) in case someone uses the hidden type all
+layouts = ['neato', 'dot', 'twopi', 'circo', 'fdp', 'nop']
 
 CC = nx.DiGraph()
 
 
-def processEven(value):
-    return value / 2
-
-
-def processOdd(value):
-    return (3 * value) + 1
-
-
-def sequence(start):
+def sequence(start, layout):
     for n in range(5, start):
         temp = n
         Ulist = [temp]
         while temp > 1:
             if temp % 2 == 0:
-                Ulist.append(processEven(temp))
+                Ulist.append(temp/2)
                 temp = Ulist[-1]
             else:
-                Ulist.append(processOdd(temp))
+                Ulist.append((3 * temp) + 1)
                 temp = Ulist[-1]
         Ulist = Ulist[0:-3]
         Ulist.append('FTO')
-        print Ulist
+#        print Ulist
         CC.add_path(Ulist)
     A = nx.to_agraph(CC)
-    A.draw('cc.png', prog='dot')
+    if layout != 'all':
+        filename = './graphs/cc' + str(start) + '-' + layout + '.png'
+        A.draw(filename, prog=layout)
+    else:
+        for layout in layouts:
+            filename = './graphs/cc' + str(start) + '-' + layout + '.png'
+            A.draw(filename, prog=layout)
+
 
 
 def main():
-    startval = input("Enter starting value: ")
-    sequence(startval)
+    sequence(startnum, gtype)
 
 
 main()
